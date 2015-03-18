8f89a3a00f887f80fe740bb2ccc4efab05e3d00f
PierreZ <pierre.zemb.isen@gmail.com>
2015-03-18 18:39:29 +0100

    channels fonctionnels

--- js/controller2.js
+++ js/controller2.js
@@ -1,3 +1,11 @@
+// Upon loading, the Google APIs JS client automatically invokes this callback.
+googleApiClientReady = function() {
+    var scope = angular.element(document.getElementsByTagName("body")).scope();
+    scope.$apply(function () {
+        scope.initiate();
+    });
+}
+
 var myApp = angular.module('youcodeio',['ngAnimate']);
 
 myApp.service('googleService', ['$http', '$rootScope', '$q', function ($http, $rootScope, $q,$scope) {
@@ -7,7 +15,7 @@ myApp.service('googleService', ['$http', '$rootScope', '$q', function ($http, $r
 	var deferred = $q.defer();
 
 // Upon loading, the Google APIs JS client automatically invokes this callback.
-this.handleClientLoad = function (q,id) {
+this.handleClientLoad = function (q,id,name) {
 	gapi.client.setApiKey(APIKEY);
 	return $q(function(resolve, reject) {
 		gapi.auth.init(function() {
@@ -22,6 +30,7 @@ this.handleClientLoad = function (q,id) {
 					maxResults: '2'
 				});
 				request.execute(function(response) {
+					response.channel_name = name;
 					resolve(response);
 				});
 			});
@@ -36,29 +45,35 @@ myApp.controller('Ctrl', function ($q, $scope, googleService) {
 	$scope.query = "";
 
 	$scope.initiate = function(){
+
 		var savePromises = [];
-		$scope.channels = youtube_channels;
-		angular.forEach($scope.channels, function(value, key){
-			savePromises[key] = googleService.handleClientLoad($scope.query,value.id);
-			$scope.parsingResults(savePromises[key]);
-			$scope.channels[key] = {name: $scope.channels[key].name, id: $scope.channels[key].id, videos: savePromises[key]};
+		$scope.unicorn = true;
+		requests = youtube_talks;
+		requests.push(youtube_tuts);
+		angular.forEach(requests, function(value,key) {
+			savePromises[key] = googleService.handleClientLoad($scope.query,value.id,value.name);
+		});
+		$q.all(savePromises).then(function(data){
+			$scope.parsingResults(data);
 		});
 	}
 
 	$scope.parsingResults = function (data){
-		videos = [];
-		angular.forEach(data, function(value) {
-			angular.forEach(value.item, function(result){
-				result.snippet.publishedAt = new Date(result.snippet.publishedAt);
-				result.snippet.id = result.id.videoId;
-				videos.push(result.snippet);	
+		$scope.channels = [];
+
+		var videos = [];
+
+		angular.forEach(data, function(value,key) {
+			$scope.channels[key] = {
+					channel_name:"",
+					channel_video: []
+				};
+			angular.forEach(value.items, function(result,key2){
+			$scope.channels[key].channel_name = result.snippet.channelTitle;
+			$scope.channels[key].channel_video.push(result.snippet);
+				
+				//$scope.channels.push(result.snippet);
 			});
-			
-		});
-		videos.sort(function(a, b) {
-			a = a.publishedAt;
-			b = b.publishedAt;
-			return a>b ? -1 : a<b ? 1 : 0;
 		});
 		$scope.unicorn = false;
 	};
