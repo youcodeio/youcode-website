eee74d1f8bf943b52ec196d01cf6480762b4e056
PierreZ <pierre.zemb.isen@gmail.com>
2015-03-31 13:44:57 +0200

    adding some milf to the project

---
+++ js/milf.js
@@ -0,0 +1,164 @@
+var conferences = [
+	{
+		"name":"Google I/O",
+		"description":"Google I/O is an annual software developer-focused conference held by Google in San Francisco, California",
+		"session" :[
+			{
+				"year":2014,
+				"link":"https://www.google.com/events/io/io14videos"
+			},
+			{
+				"year":2013,
+				"link":"https://developers.google.com/events/io/"
+			}
+		]
+	},
+	{
+		"name":"DotGo",
+		"description":"The European Go conference",
+		"session" :[
+			{
+				"year":2014,
+				"link":"&query=dotgo%202014"
+			}
+		]
+	},
+	{
+		"name":"DotJS",
+		"description":"The European JS conference",
+		"session" :[
+			{
+				"year":2014,
+				"link":"&query=dotjs%202014"
+			}
+		]
+	},
+	{
+		"name":"DotScale",
+		"description":"The European Tech Conference on Scalability",
+		"session" :[
+			{
+				"year":2014,
+				"link":"&query=dotscale%202014"
+			},
+			{
+				"year":2013,
+				"link":"&query=dotscale%202013"
+			}
+		]
+	},
+	{
+		"name":"dotRB",
+		"description":"The European Ruby conference",
+		"session" :[
+			{
+				"year":2013,
+				"link":"&query=dotruby%202013"
+			}
+		]
+	},
+	{
+		"name":"dotCSS",
+		"description":"The largest CSS conference in Europe",
+		"session" :[
+			{
+				"year":2014,
+				"link":"&query=dotscale%202014"
+			}
+		]
+	},
+	{
+		"name":"dotSwift",
+		"description":"The largest CSS conference in Europe",
+		"session" :[
+			{
+				"year":2015,
+				"link":"&query=dotscale%20205"
+			}
+		]
+	},
+	{
+		"name":"FOSDEM",
+		"description":"FOSDEM is a free event for software developers to meet, share ideas and collaborate.",
+		"session" :[
+			{
+				"year":2015,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2014,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2013,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2012,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2011,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2010,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2009,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2008,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2007,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2006,
+				"link":"https://video.fosdem.org/"
+			},
+			{
+				"year":2005,
+				"link":"https://video.fosdem.org/"
+			},
+		]
+	},
+	{
+		"name":"ng-conf",
+		"description":"The World's Original Angular Conference",
+		"session" :[
+			{
+				"year":2014,
+				"link":"&query=dotscale%20205"
+			}
+		]
+	},
+	{
+		"name":"DConf",
+		"description":"The World's Original Angular Conference",
+		"session" :[
+			{
+				"year":2014,
+				"link":"&query=dconf%20205"
+			}
+		]
+	},
+	{
+		"name":"Devoxx",
+		"description":"Devoxx",
+		"session" :[
+			{
+				"year":2014,
+				"link":"https://www.parleys.com/channel/devoxx-france-2014"
+			},
+			{
+				"year":2013,
+				"link":"https://www.parleys.com/channel/devoxx-2013"
+			}
+		]
+	},
+];
