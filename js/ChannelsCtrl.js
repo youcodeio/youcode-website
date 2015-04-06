707b81fb5e009980607a5010265729c9a15d9ad3
root <root@allxs-VirtualBox.(none)>
2015-04-06 19:53:49 +0200

    channels new visual click on channel and dyn channel selected

--- js/ChannelsCtrl.js
+++ js/ChannelsCtrl.js
@@ -6,6 +6,7 @@ angular.module('youcodeio.controllers.channels', [])
 
 		console.log("initiate");
 
+		var bannerImageUrl;
 		var banners = [];
 		var savePromises = [];
 		$scope.unicorn = true;
@@ -27,6 +28,7 @@ angular.module('youcodeio.controllers.channels', [])
 			});
 			$q.all(savePromises).then(function(data){
 				$scope.parsingResults(data, d);
+				$scope.show_channel = $scope.channels[0];
 			});
 		});
 	
@@ -41,27 +43,37 @@ angular.module('youcodeio.controllers.channels', [])
 					channel_name:d[key].items[0].snippet.title,
 					channel_logo: d[key].items[0].snippet.thumbnails.high,
 					channel_banner: d[key].items[0].brandingSettings.image.bannerImageUrl,
+					channel_link: "https://www.youtube.com/channel/"+d[key].items[0].id,
 					channel_video: []
 				};
 			angular.forEach(value.items, function(result,key2){
 				var video = {
 					video_title: result.snippet.title,
 					video_desc: result.snippet.description,
-					video_publishedAt: result.snippet.publishedAt,
+					video_publishedAt: new Date(result.snippet.publishedAt),
 					video_thumbnails: result.snippet.thumbnails,
 					video_link: "https://www.youtube.com/watch?v="+result.id.videoId
 				};
 				$scope.channels[key].channel_video.push(video);
-				console.log($scope.channels[key].channel_name);
-				console.log(video.video_thumbnails.high.url);
-				if(video.video_thumbnails.high.url == ""){
-					console.log($scope.channels[key].name);
-				}
 			});
 		});
 		$scope.unicorn = false;
 	};
-})
 
+	$scope.changePrimary = function(channelObject){
+		console.log(channelObject);
+		$scope.show_channel = channelObject;
+		
+		console.log($scope.show_channel);
+	};
+});
 
+app.directive('backImg', function(){
+	return function(scope, elements, attrs){
+		var url = attrs.backImg;
+		element.css({
+    		'background-image': 'url(' + url +')'
+		});
+	};
+});
 
